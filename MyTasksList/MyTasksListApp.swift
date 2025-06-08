19d939678d3e20516f4d89664a7c204efc20a32d
Tony Pham <thanh01.pmt@gmail.com>
2025-06-08 11:23:30 +0700

    Initial Commit

---
+++ MyTasksList/MyTasksListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTasksListApp.swift
+//  MyTasksList
+//
+//  Created by Tony Pham on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTasksListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
